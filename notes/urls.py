278b0b2456cb52bc1c8f9fcb1e266635cb2cf575
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-17 19:46:55 +0530

    added upadte option

--- notes/urls.py
+++ notes/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create_note/',views.create_note,name="create_note"),
-    path('delete/<int:id>/', views.delete_note, name='delete_note')
+    path('delete/<int:id>/', views.delete_note, name='delete_note'),
+    path('edit_note/<int:id>/', views.edit_note, name='edit_note')
+
 
 ]
 
