b130ab2f594340a14844ca9afbb939788bb18044
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-17 19:00:04 +0530

    added delete feature

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create_note/',views.create_note,name="create_note")
+    path('create_note/',views.create_note,name="create_note"),
+    path('delete/<int:id>/', views.delete_note, name='delete_note')
+
 ]
 
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Note
 from .forms import NoteForm
 
@@ -15,3 +15,12 @@ def create_note(request):
             form.save()
             return redirect('home')
     
+def delete_note(request,id):
+    if request.method=='POST':
+        note = get_object_or_404(Note, id=id)
+        note.delete()
+        return redirect('home')
+        
+
+
+
