e528423ebaaa86b98e47aa708d7d9665e4351b2e
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-17 18:11:26 +0530

    added get request

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Note
+from .forms import NoteForm
+
 
 def home(request):
     notes = Note.objects.all()
     return render(request, 'notes/home.html', {'notes': notes})
+
+
+def create_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
