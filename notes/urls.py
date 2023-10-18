6f12855fdf522a19aa72be65ac9c3b022c354cb2
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-19 01:12:41 +0530

    feat : image

--- notes/urls.py
+++ notes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('edit_note/<int:id>/', views.edit_note, name='edit_note'),
     path('view_note/<int:id>',views.view_note,name="view_note")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
